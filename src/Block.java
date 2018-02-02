f36061dfcc70de27b4bc7141b8b69f6cc07faa91
CKKnot <dalvin.cat@gmail.com>
2018-02-02 23:13:49 +0800

    Election of blockmaster is implemented with simple test on Main.java

--- src/Block.java
+++ src/Block.java
@@ -2,20 +2,20 @@ import java.util.Date;
 
 public class Block {
 
-	public String hash;
-	public String previousHash;
+	String previousHash;
+	long blockHeight;
 	private String data;
 	private long timeStamp;
 
 	//Block Constructor.
-	public Block(String data,String previousHash ) {
+	public Block(String data,String previousHash) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
-		this.hash = calculateHash(); 
+		this.blockHeight = Main.getBlockHeight() + 1;
 	}
 	
-	
+	//Return the hash of the current block
 	public String calculateHash() {
 		String calculatedhash = HashFunctionUtility.applySha256( 
 				previousHash +
@@ -23,5 +23,10 @@ public class Block {
 				data 
 				);
 		return calculatedhash;
+	}
+	
+	//Return the height of the current block
+	public long getBlockHeight() {
+		return blockHeight;
 	}
 }
--- src/HashFunctionUtility.java
+++ src/HashFunctionUtility.java
@@ -20,5 +20,4 @@ public class HashFunctionUtility {
 				throw new RuntimeException(e);
 			}
 	} 
-
 }

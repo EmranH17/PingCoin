ee89754c2e8ed4f1a4b22c3cbe4318d2e3a2f2cb
miller619 <4310713@students.swinburne.edu.my>
2018-02-02 16:32:14 +0600

    blocks created, hashad with previous block hash

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 
 public class Main {
 
 	//Simple example of how to use BlockGenerator module
 	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
+	
 	public static void main(String[] args) {
 		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-		LocalDateTime now = LocalDateTime.now();
-		String dtStr = dtf.format(now);
-		
-		BlockGenerator.compileBlock(Long.parseLong(dtStr), 0, "c9a2ebe3c4a631b66d03095553db3370466d5fb8122ce1485992e83338864947", "b42c47ef6b54655f061cb40201eaf68e7c97abc6b8f12584526585347663bfd1");
+		//add our blocks to the block chain ArrayList:
+				blockchain.add(new Block("Genesis", "0"));		
+				blockchain.add(new Block("2nd block",blockchain.get(blockchain.size()-1).hash)); 
+				blockchain.add(new Block("3rd block",blockchain.get(blockchain.size()-1).hash));
+				
+				String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+				System.out.println(blockchainJson);
 	}
 
 }

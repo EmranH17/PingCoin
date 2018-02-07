63d21890cf73f5f05a1b3fc251c51619f4ed3783
CKKnot <dalvin.cat@gmail.com>
2018-02-07 11:06:05 +0800

    1. Arranged parameter structure 2.Added blockHash parameter 3. Added method getRawBlock() to return the parameters to String

--- src/Block.java
+++ src/Block.java
@@ -1,30 +1,32 @@
 import java.util.Date;
 
 public class Block {
-
-	String previousHash;
-	long blockHeight;
-	private String data;
-	private long timeStamp;
 	
-	
+	public String blockHash;
+	public long timeStamp;
+	public long blockHeight;
+	public String previousHash;
+	public String data;
 
 	//Block Constructor.
 	public Block(String data,String previousHash) {
-		this.data = data;
-		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
-		this.blockHeight = Main.getBlockHeight() + 1;		
-	}
-	
-	//Return the hash of the current block
-	public String calculateHash() {
-		String calculatedhash = HashFunctionUtility.applySha256(previousHash + Long.toString(timeStamp) + data);
-		return calculatedhash;
+		this.blockHeight = Main.blockchain.size();
+		this.previousHash = previousHash;
+		this.data = data;
+		this.blockHash = HashFunctionUtility.applySha256(Long.toString(timeStamp) + blockHeight + this.previousHash + this.data);
 	}
 	
 	//Return the height of the current block
 	public long getBlockHeight() {
 		return blockHeight;
 	}
+	
+	//Return the raw String representation of the block, useful when saving the block or sending it to a peer. 
+	public String getRawBlock()
+    {
+        String rawBlock = ""; 
+        rawBlock = "{" + timeStamp + ":" + blockHeight + ":" + previousHash + ":" + data + "}";
+        return rawBlock; 
+    }
 }

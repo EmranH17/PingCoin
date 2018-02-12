8d289552bda02ee1fd84eccc2c485a10af36a2a4
CKKnot <dalvin.cat@gmail.com>
2018-02-12 10:41:35 +0800

    Make use of the Blockchain class instead of ArrayList<Block>

--- src/blockMasterElection.java
+++ src/blockMasterElection.java
@@ -1,13 +1,12 @@
 import java.security.SecureRandom;
-import java.util.ArrayList;
 
 public class blockMasterElection {
 	Block electedBlock;
 	SecureRandom randomizer = new SecureRandom();
 	
 	//Elect a block randomly from array of blocks given
-	public Block electBlock(ArrayList<Block> activeBlock) {
-		electedBlock = activeBlock.get( randomizer.nextInt( activeBlock.size() ) );
+	public Block electBlock(Blockchain blockchain) {
+		electedBlock = blockchain.get( randomizer.nextInt( blockchain.size() ) );
 		return electedBlock;
 	}
 }

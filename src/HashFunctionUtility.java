7894018ddfd7902d0996ac1ef067f3bdf25b9115
CKKnot <dalvin.cat@gmail.com>
2018-02-20 11:36:32 +0800

    A method byteToString() is added to HashFunctionUtility class to simplify btye conversion

--- src/HashFunctionUtility.java
+++ src/HashFunctionUtility.java
@@ -58,4 +58,14 @@ public class HashFunctionUtility {
 		public static String getStringFromKey(Key key) {
 			return Base64.getEncoder().encodeToString(key.getEncoded());
 	}
+		
+		public static String byteToString(byte[] bytes) {
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < bytes.length; i++) {
+				String hex = Integer.toHexString(0xff & bytes[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		}
 }
